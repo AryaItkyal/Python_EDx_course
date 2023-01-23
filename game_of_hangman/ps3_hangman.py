85a769a1625a785df8672bf8019aae28d5d41cea
AryaItkyal <69670481+AryaItkyal@users.noreply.github.com>
2023-01-23 09:32:44 +0530

    Update ps3_hangman.py

--- game_of_hangman/ps3_hangman.py
+++ game_of_hangman/ps3_hangman.py
@@ -1,11 +1,5 @@
 # Hangman game
-#
 
-# -----------------------------------
-# Helper code
-# You don't need to understand this helper code,
-# but you will have to know how to use the functions
-# (so be sure to read the docstrings!)
 
 import random
 
@@ -36,11 +30,7 @@ def chooseWord(wordlist):
     """
     return random.choice(wordlist)
 
-# end of helper code
-# -----------------------------------
 
-# Load the list of words into the variable wordlist
-# so that it can be accessed from anywhere in the program
 wordlist = loadWords()
 
 def isWordGuessed(secretWord, lettersGuessed):
@@ -142,13 +132,6 @@ def hangman(secretWord):
     return 
 
 
-
-
-
-
-# When you've completed your hangman function, uncomment these two lines
-# and run this file to test! (hint: you might want to pick your own
-# secretWord while you're testing)
 
 secretWord = chooseWord(wordlist).lower()
 hangman(secretWord)
